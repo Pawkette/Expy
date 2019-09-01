34c338ecf72d2c19fd4b020ca3646cda75372fc2
Sammy James <sammy.james@protonmail.com>
2019-09-01 15:51:19 -0700

    hey shit works now :poop:

--- Core.lua
+++ Core.lua
@@ -50,18 +50,18 @@ local AddonTitle = select(2, GetAddOnInfo(AddonName))
 ---
 -- Addon
 ---
-local Expy          = LibStub( 'AceAddon-3.0' ):NewAddon( 'Expy', 'AceConsole-3.0', 'AceEvent-3.0' )
-Expy._CurrentXP     = 0
-Expy._MaxXP         = 0
-Expy._DeltaXP       = 0
-Expy._Level         = 0
-Expy._XPEnabled     = true
-Expy._Invalid       = {}
-Expy._Frame         = nil
-Expy._Resting       = PlayerStates.NORMAL
-Expy._RestedXP      = 0
-Expy._Mode          = Modes.EXPERIENCE
-Expy._UpdatePending = false
+local Expy           = LibStub( 'AceAddon-3.0' ):NewAddon( 'Expy', 'AceConsole-3.0', 'AceEvent-3.0' )
+Expy.m_CurrentXP     = 0
+Expy.m_MaxXP         = 0
+Expy.m_DeltaXP       = 0
+Expy.m_Level         = 0
+Expy.m_XPEnabled     = true
+Expy.m_Invalid       = {}
+Expy.m_Frame         = nil
+Expy.m_Resting       = PlayerStates.NORMAL
+Expy.m_RestedXP      = 0
+Expy.m_Mode          = Modes.EXPERIENCE
+Expy.m_UpdatePending = false
 
 local LibConfigDialog = LibStub( 'AceConfigDialog-3.0' )
 local LibSmooth       = LibStub( 'LibSmoothStatusBar-1.0' )
@@ -80,7 +80,7 @@ local options = {
             type          = 'select',
             dialogControl = 'LSM30_Statusbar',
             values        = LibSM:HashTable( 'statusbar' ),
-            get           = function() return Expy._db.texture end,
+            get           = function() return Expy.db.global.texture end,
             set           = function(self, key) Expy:SetTexture( key ) end,
         },
         font = {
@@ -88,7 +88,7 @@ local options = {
             type          = 'select',
             dialogControl = 'LSM30_Font',
             values        = LibSM:HashTable( 'font' ),
-            get           = function() return Expy._db.font end,
+            get           = function() return Expy.db.global.font end,
             set           = function(self, key) Expy:SetFont( key ) end,
         },
     },
@@ -96,38 +96,38 @@ local options = {
 
 local OptionsTable = LibStub( 'AceConfig-3.0' ):RegisterOptionsTable( 'Expy', options, {'expy'} )
 
-local defaults = {
-    profile = {
-        texture = LibSM:GetDefault( 'statusbar' ),
-        font = LibSM:GetDefault( 'font' ),
-    }
-}
-
 ---
 -- Called when the addon is initialized
 --
 function Expy:OnInitialize()
-    self._db = LibDB:New( AddonName .. 'DB', defaults ) 
+    local defaults = {
+        profile = {
+            texture = LibSM:GetDefault( 'statusbar' ),
+            font = LibSM:GetDefault( 'font' ),
+        }
+    }
+
+    self.db = LibDB:New( AddonName .. 'DB', defaults, true ) 
     LibConfigDialog:AddToBlizOptions( AddonName, AddonTitle )
 end
 
 function Expy:SetTexture( new_texture )
-    self._db.texture = new_texture
+    self.db.global.texture = new_texture
 
     local statusbar = LibSM:Fetch( 'statusbar', new_texture )
 
-    self._RestBar:SetStatusBarTexture( statusbar )
-    self._XPBar:SetStatusBarTexture( statusbar )
+    self.m_RestBar:SetStatusBarTexture( statusbar )
+    self.m_XPBar:SetStatusBarTexture( statusbar )
 end
 
 function Expy:SetFont( new_font )
-    self._db.font = new_font
+    self.db.global.font = new_font
 
     local font = LibSM:Fetch( 'font', new_font )
 
-    self._Textfield:SetFont( font, 10, nil )
-    self._LevelField:SetFont( font, 16, nil )
-    self._Percent:SetFont( font, 10, nil )
+    self.m_Textfield:SetFont( font, 10, nil )
+    self.m_LevelField:SetFont( font, 16, nil )
+    self.m_Percent:SetFont( font, 10, nil )
 end
 
 ---
@@ -155,68 +155,68 @@ end
 -- Set up the frame!
 --
 function Expy:InitializeFrame()
-    local statusbar = LibSM:Fetch( 'statusbar', self._db.texture or LibSM:GetDefault( 'statusbar' ) )
-    local font      = LibSM:Fetch( 'font', self._db.font or LibSM:GetDefault( 'font' ) )
+    local statusbar = LibSM:Fetch( 'statusbar', self.db.global.texture or LibSM:GetDefault( 'statusbar' ) )
+    local font      = LibSM:Fetch( 'font', self.db.global.font or LibSM:GetDefault( 'font' ) )
 
-    self._Frame = CreateFrame( 'Frame', 'Expy', UIParent )
-    self._Frame._Parent = self
+    self.m_Frame = CreateFrame( 'Frame', 'Expy', UIParent )
+    self.m_Frame.m_Parent = self
 
-    self._Frame:SetFrameStrata( 'HIGH' )
-    self._Frame:SetMovable( false )
+    self.m_Frame:SetFrameStrata( 'HIGH' )
+    self.m_Frame:SetMovable( false )
 
-    self._Frame:ClearAllPoints()
-    self._Frame:SetPoint( 'BOTTOM', UIParent, 'BOTTOM', 0, 0 )
-    self._Frame:SetWidth( UIParent:GetWidth() )
-    self._Frame:SetHeight( FRAME_HEIGHT )
+    self.m_Frame:ClearAllPoints()
+    self.m_Frame:SetPoint( 'BOTTOM', UIParent, 'BOTTOM', 0, 0 )
+    self.m_Frame:SetWidth( UIParent:GetWidth() )
+    self.m_Frame:SetHeight( FRAME_HEIGHT )
 
-    self._Frame:SetBackdrop( {
+    self.m_Frame:SetBackdrop( {
         bgFile = statusbar,
     } )
 
-    self._Frame:SetBackdropColor( 0.25, 0.25, 0.25, 0.75 )
+    self.m_Frame:SetBackdropColor( 0.25, 0.25, 0.25, 0.75 )
 
-    self._RestBar = CreateFrame( 'StatusBar', 'Expy.RestBar', self._Frame ) 
-    self._RestBar:SetStatusBarTexture( statusbar )
-    self._RestBar:SetStatusBarColor( 0.12, 0.69, 0.06, 0.75 )
-    self._RestBar:SetPoint( 'TOPLEFT', self._Frame, 'TOPLEFT', -1, -1 )
-    self._RestBar:SetPoint( 'BOTTOMRIGHT', self._Frame, 'BOTTOMRIGHT', 1, 1 )
-    self._RestBar:SetMinMaxValues( 0.0, 1.0 )
+    self.m_RestBar = CreateFrame( 'StatusBar', 'Expy.RestBar', self.m_Frame ) 
+    self.m_RestBar:SetStatusBarTexture( statusbar )
+    self.m_RestBar:SetStatusBarColor( 0.12, 0.69, 0.06, 0.75 )
+    self.m_RestBar:SetPoint( 'TOPLEFT', self.m_Frame, 'TOPLEFT', -1, -1 )
+    self.m_RestBar:SetPoint( 'BOTTOMRIGHT', self.m_Frame, 'BOTTOMRIGHT', 1, 1 )
+    self.m_RestBar:SetMinMaxValues( 0.0, 1.0 )
 
-    LibSmooth:SmoothBar( self._RestBar )
+    LibSmooth:SmoothBar( self.m_RestBar )
 
-    self._XPBar = CreateFrame( 'StatusBar', 'Expy.XPBar', self._Frame )
-    self._XPBar:SetStatusBarTexture( statusbar )
-    self._XPBar:SetStatusBarColor( 0.98, 0.84, 0.09, 1.0 )
-    self._XPBar:SetPoint( 'TOPLEFT', self._Frame, 'TOPLEFT', -1, -1 )
-    self._XPBar:SetPoint( 'BOTTOMRIGHT', self._Frame, 'BOTTOMRIGHT', 1, 1 )
-    self._XPBar:SetMinMaxValues( 0.0, 1.0 )
+    self.m_XPBar = CreateFrame( 'StatusBar', 'Expy.XPBar', self.m_Frame )
+    self.m_XPBar:SetStatusBarTexture( statusbar )
+    self.m_XPBar:SetStatusBarColor( 0.98, 0.84, 0.09, 1.0 )
+    self.m_XPBar:SetPoint( 'TOPLEFT', self.m_Frame, 'TOPLEFT', -1, -1 )
+    self.m_XPBar:SetPoint( 'BOTTOMRIGHT', self.m_Frame, 'BOTTOMRIGHT', 1, 1 )
+    self.m_XPBar:SetMinMaxValues( 0.0, 1.0 )
 
-    LibSmooth:SmoothBar( self._XPBar )
+    LibSmooth:SmoothBar( self.m_XPBar )
 
-    self._Textfield = self._Frame:CreateFontString( 'Expy.Text', 'OVERLAY' )
-    self._Textfield:SetShadowOffset( 1, -1 )
-    self._Textfield:SetTextColor( 1, 1, 1, 1 )
-    self._Textfield:SetPoint( 'BOTTOMLEFT', self._Frame, 'TOPLEFT', 2, 2 )
-    self._Textfield:SetFont( font, 10, nil )
+    self.m_Textfield = self.m_Frame:CreateFontString( 'Expy.Text', 'OVERLAY' )
+    self.m_Textfield:SetShadowOffset( 1, -1 )
+    self.m_Textfield:SetTextColor( 1, 1, 1, 1 )
+    self.m_Textfield:SetPoint( 'BOTTOMLEFT', self.m_Frame, 'TOPLEFT', 2, 2 )
+    self.m_Textfield:SetFont( font, 10, nil )
 
-    self._LevelField = self._Frame:CreateFontString( 'Expy.Level', 'OVERLAY' )
-    self._LevelField:SetShadowOffset( 1, -1 )
-    self._LevelField:SetTextColor( 1, 1, 1, 1 )
-    self._LevelField:SetPoint( 'BOTTOMLEFT', self._Textfield, 'TOPLEFT', 0, 0 )
-    self._LevelField:SetFont( font, 16, nil )
+    self.m_LevelField = self.m_Frame:CreateFontString( 'Expy.Level', 'OVERLAY' )
+    self.m_LevelField:SetShadowOffset( 1, -1 )
+    self.m_LevelField:SetTextColor( 1, 1, 1, 1 )
+    self.m_LevelField:SetPoint( 'BOTTOMLEFT', self.m_Textfield, 'TOPLEFT', 0, 0 )
+    self.m_LevelField:SetFont( font, 16, nil )
 
-    self._Percent = self._Frame:CreateFontString( 'Expy.Percent', 'OVERLAY' )
-    self._Percent:SetShadowOffset( 1, -1 )
-    self._Percent:SetTextColor( 1, 1, 1, 1 )
-    self._Percent:SetPoint( 'LEFT', self._Textfield, 'RIGHT', 2, 0 )
-    self._Percent:SetFont( font, 10, nil )
+    self.m_Percent = self.m_Frame:CreateFontString( 'Expy.Percent', 'OVERLAY' )
+    self.m_Percent:SetShadowOffset( 1, -1 )
+    self.m_Percent:SetTextColor( 1, 1, 1, 1 )
+    self.m_Percent:SetPoint( 'LEFT', self.m_Textfield, 'RIGHT', 2, 0 )
+    self.m_Percent:SetFont( font, 10, nil )
 end
 
 ---
 -- Called when this addon is disabled
 --
 function Expy:OnDisable()
-    self._Frame:Hide()
+    self.m_Frame:Hide()
 end
 
 ---
@@ -231,9 +231,9 @@ end
 -- called to switch between xp and rep mode
 --
 function Expy:SetMode( new_mode )
-    if (new_mode ~= self._Mode) then
+    if (new_mode ~= self.m_Mode) then
         self:Invalidate( InvalidationTypes.MODE )
-        self._Mode = new_mode
+        self.m_Mode = new_mode
     end
 end
 
@@ -247,10 +247,10 @@ function Expy:OnUpdate()
 
     if ( self:IsInvalid( InvalidationTypes.LEVEL ) ) then
         local tracked = self:GetTracked()
-        if ( tracked ~= nil and type(tracked) == 'string' ) then
-            self._LevelField:SetText( tracked )
+        if ( tracked ~= nil and type( tracked ) == 'string' ) then
+            self.m_LevelField:SetText( tracked )
         else 
-            self._LevelField:SetText( 'No Faction Tracked' )
+            self.m_LevelField:SetText( 'No Faction Tracked' )
         end
     end
 
@@ -258,40 +258,42 @@ function Expy:OnUpdate()
         local progressText = self:GetProgressText()
         local progressScalar = self:GetProgressScalar()
 
-        if ( progressScalar ~= nil and type(progressScalar) == 'number' ) then
-            self._XPBar:SetValue( progressScalar )
-            self._Percent:SetText( '( ' .. floor( progressScalar * 100.0 ) .. '% )' )
+        if ( progressScalar ~= nil and type( progressScalar ) == 'number' ) then
+            self.m_XPBar:SetValue( progressScalar )
+            self.m_Percent:SetText( '( ' .. floor( progressScalar * 100.0 ) .. '% )' )
         end
 
-        if ( progressText ~= nil and type(progressText) == 'string' ) then
-            self._Textfield:SetText( self:GetProgressText() )
+        if ( progressText ~= nil and type( progressText ) == 'string' ) then
+            self.m_Textfield:SetText( progressText )
         end
 
-        if ( self._RestedXP == nil or self._RestedXP == 0 ) then
-            self._RestBar:SetValue( 0.0 )
+        if ( self.m_RestedXP == nil or self.m_RestedXP == 0 ) then
+            self.m_RestBar:SetValue( 0.0 )
+        else
+            self.m_RestBar:SetValue( ( self.m_CurrentXP + ( self.m_RestedXP or 0 ) ) / self.m_MaxXP )
         end
     end
 
-    if ( self._Mode == Modes.EXPERIENCE ) then
+    if ( self.m_Mode == Modes.EXPERIENCE ) then
         if ( self:IsInvalid( InvalidationTypes.REST_XP ) ) then
-            self._RestBar:SetValue( ( self._CurrentXP + ( self._RestedXP or 0 ) ) / self._MaxXP )
+            self.m_RestBar:SetValue( ( self.m_CurrentXP + ( self.m_RestedXP or 0 ) ) / self.m_MaxXP )
         end
         
         if ( self:IsInvalid( InvalidationTypes.REST_STATE ) ) then
-            if ( self._Resting == PlayerStates.RESTING ) then
-                self._RestBar:SetStatusBarColor( 0.09, 0.47, 0.98, 0.75 )
+            if ( self.m_Resting == PlayerStates.RESTING ) then
+                self.m_RestBar:SetStatusBarColor( 0.09, 0.47, 0.98, 0.75 )
             else
-                self._RestBar:SetStatusBarColor( 0.12, 0.69, 0.06, 0.75 )
+                self.m_RestBar:SetStatusBarColor( 0.12, 0.69, 0.06, 0.75 )
             end
         end
     else
-        self._RestBar:SetValue( 0.0 )
+        self.m_RestBar:SetValue( 0.0 )
     end
 
     -- revert all these to default
-    self._Invalid = {}
-    self._Frame:SetScript( 'OnUpdate', nil )
-    self._UpdatePending = false
+    self.m_Invalid = {}
+    self.m_Frame:SetScript( 'OnUpdate', nil )
+    self.m_UpdatePending = false
 end
 
 ---
@@ -299,8 +301,8 @@ end
 --
 function Expy:RequestUpdate()
     if ( not self._UpdatePending ) then
-        self._Frame:SetScript( 'OnUpdate', function( self, _ ) self._Parent:OnUpdate() end )
-        self._UpdatePending = true
+        self.m_Frame:SetScript( 'OnUpdate', function( self, _ ) self.m_Parent:OnUpdate() end )
+        self.m_UpdatePending = true
     end
 end
 
@@ -312,12 +314,12 @@ function Expy:Invalidate( types )
     if ( type( types ) == 'table' ) then
         for i=1,#types,1 do 
             if ( not self:IsInvalid( types[ i ] ) ) then
-                insert( self._Invalid, types[ i ] )
+                insert( self.m_Invalid, types[ i ] )
             end
         end
     elseif ( type( types ) == 'number' ) then
         if ( not self:IsInvalid( types ) ) then
-            insert( self._Invalid, types )
+            insert( self.m_Invalid, types )
         end
     end
 
@@ -331,10 +333,10 @@ end
 -- @param type the type to check
 function Expy:IsInvalid( type )
     if ( not type ) then 
-        return #self._Invalid ~= 0
+        return #self.m_Invalid ~= 0
     end
 
-    for _,v in pairs( self._Invalid ) do
+    for _,v in pairs( self.m_Invalid ) do
         if ( v == type ) then
             return true
         end
@@ -351,7 +353,7 @@ end
 -- Called when the player disables XP
 --
 function Expy:HandleXPDisabled()
-    self._XPEnabled = false
+    self.m_XPEnabled = false
 
     self:Invalidate( InvalidationTypes.XP )
 end
@@ -360,7 +362,7 @@ end
 -- Called when the player enables XP
 --
 function Expy:HandleXPEnabled()
-    self._XPEnabled = true
+    self.m_XPEnabled = true
 
     self:Invalidate( InvalidationTypes.XP )
 end
@@ -369,10 +371,10 @@ end
 -- Called when a unit's xp changes
 --
 function Expy:HandleXPUpdate()
-    local PreviousXP    = self._CurrentXP
-
-    self._CurrentXP     = UnitXP( 'player' )
-    self._DeltaXP       = self._CurrentXP - PreviousXP
+    local PreviousXP    = self.m_CurrentXP
+    self.m_CurrentXP     = UnitXP( 'player' )
+    self.m_MaxXP         = UnitXPMax( 'player' )
+    self.m_DeltaXP       = self.m_CurrentXP - PreviousXP
 
     self:Invalidate( InvalidationTypes.XP )
 end
@@ -383,10 +385,10 @@ end
 -- @param level the player's level
 --
 function Expy:HandleLevelUp( _, level )
-    self._Level = level
-    self._MaxXP = UnitXPMax( 'player' )
+    self.m_Level = level
+    self.m_MaxXP = UnitXPMax( 'player' )
 
-    if ( self._Level == MAX_LEVEL ) then
+    if ( self.m_Level == MAX_LEVEL ) then
         self:SetMode( Modes.REPUTATION )
     end
     self:Invalidate( InvalidationTypes.LEVEL )
@@ -397,9 +399,9 @@ end
 -- 
 function Expy:HandleRestingUpdate()
     if ( IsResting() ) then
-        self._Resting = PlayerStates.RESTING
+        self.m_Resting = PlayerStates.RESTING
     else
-        self._Resting = PlayerStates.NORMAL 
+        self.m_Resting = PlayerStates.NORMAL 
     end
 
     self:Invalidate( InvalidationTypes.REST_STATE ) 
@@ -409,7 +411,7 @@ end
 -- Called when the player's rested xp changes
 --
 function Expy:HandleRestingXPUpdate()
-    self._RestedXP = GetXPExhaustion()
+    self.m_RestedXP = GetXPExhaustion()
 
     self:Invalidate( InvalidationTypes.REST_XP )
 end
@@ -426,9 +428,9 @@ end
 -- getters
 --
 function Expy:GetTracked() 
-    if ( self._Mode == Modes.EXPERIENCE ) then
-        return 'Lv ' .. self._Level
-    elseif ( self._Mode == Modes.REPUTATION ) then
+    if ( self.m_Mode == Modes.EXPERIENCE ) then
+        return 'Lv ' .. self.m_Level
+    elseif ( self.m_Mode == Modes.REPUTATION ) then
         local name, standing, _, _, _ = GetWatchedFactionInfo()
         if ( name ~= nil and standing ~= nil ) then
             return name .. ' (' .. _G[ 'FACTION_STANDING_LABEL' .. standing ] .. ')'
@@ -439,9 +441,9 @@ function Expy:GetTracked()
 end
 
 function Expy:GetProgressScalar()
-    if ( self._Mode == Modes.EXPERIENCE ) then
-        return self._CurrentXP / self._MaxXP
-    elseif ( self._Mode == Modes.REPUTATION ) then
+    if ( self.m_Mode == Modes.EXPERIENCE ) then
+        return self.m_CurrentXP / self.m_MaxXP
+    elseif ( self.m_Mode == Modes.REPUTATION ) then
         local _, _, min, max, value = GetWatchedFactionInfo()
         if ( min ~= nil and max ~= nil and value ~= nil ) then
             return (value - min) / (max - min)
@@ -452,9 +454,9 @@ function Expy:GetProgressScalar()
 end
 
 function Expy:GetProgressText()
-    if ( self._Mode == Modes.EXPERIENCE ) then
-        return  self._CurrentXP .. ' / ' .. self._MaxXP
-    elseif ( self._Mode == Modes.REPUTATION ) then
+    if ( self.m_Mode == Modes.EXPERIENCE ) then
+        return  self.m_CurrentXP .. ' / ' .. self.m_MaxXP
+    elseif ( self.m_Mode == Modes.REPUTATION ) then
         local _, _, min, max, value = GetWatchedFactionInfo()
         if ( min ~= nil and max ~= nil and value ~= nil ) then
             return (value - min) .. ' / ' .. (max - min)
